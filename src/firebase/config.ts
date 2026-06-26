9b9d16a1013c1c8d551be0e881e1c405a4128ed9
kuldeepbalara0814 <kuldeep.balara0814@gmail.com>
2026-06-26 13:09:55 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0JdILxLtX7QAwUizDxV2Imb96_sCpq88",
+  authDomain: "zesty-case-12t1j.firebaseapp.com",
+  projectId: "zesty-case-12t1j",
+  storageBucket: "zesty-case-12t1j.firebasestorage.app",
+  messagingSenderId: "1053905583961",
+  appId: "1:1053905583961:web:4c31fe2c6ae8fe14bb5648",
+  measurementId: ""
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+
+// यहाँ हमने आपका खास Database ID डाल दिया है, जिससे डेटाबेस सही से कनेक्ट होगा
+export const db = getFirestore(app, "ai-studio-c7f160bc-3419-4bd9-9bf4-816c1c7b4581");

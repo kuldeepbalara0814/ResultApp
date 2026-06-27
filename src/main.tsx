69c68e425b27839258961849da05c7aa86b22dfc
kuldeepbalara0814 <kuldeep.balara0814@gmail.com>
2026-06-27 23:31:28 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ErrorBoundary } from './components/ErrorBoundary.tsx';
+import ErrorBoundary from './components/ErrorBoundary.tsx'; // यहाँ से { } हटा दिए गए हैं
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
